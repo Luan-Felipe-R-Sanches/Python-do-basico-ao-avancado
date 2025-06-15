04168c13459ffbe686a2adbe269c3599dccc3e28
Luan-Felipe-R-Sanches <luanfeliperodrigues25@gmail.com>
2025-06-15 10:51:51 -0400

    Projeto inicial com sintaxe, estrutura e exercícios de Python

---
+++ 2_SINTAXE_E_ESTRUTURA/1_iniciando.py
@@ -0,0 +1,29 @@
+# Aula 1
+if saudacao():
+    print("Minha saudação")
+    
+if __name__=="__main__":
+    saudacao()
+    
+if 10 > 5:
+    print("10 realmente é maior que 5")
+    print("Operação finalizada")
+    print("Terminando o if")
+    
+# Aula 3 - Blocos em Python
+if 70 > 18:
+    print("Você é maior de idade")
+    # Verifica se o usúario é idoso e mostra uma mensagem
+    if 70 >65:
+        print("Você é considerado idoso")
+
+# Aula 4 - Comentários
+
+# Aula 5 - Função print
+print("Exibir mensagem")
+
+print("Exibir", "Mensagem")
+
+print("Laranja", "Maça", "Banana", sep=" - ")
+print("Carregando", end="...")
+
